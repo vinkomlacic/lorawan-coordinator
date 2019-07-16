a19063df0fd0ef44a9d146bec912264063a1943c
Vinko Mlačić <vinkomlacic@outlook.com>
2019-07-16 22:07:27 +0200

    Update configuration JS object

--- src/app_config/Configuration.js
+++ src/app_config/Configuration.js
@@ -3,7 +3,6 @@ const ConfigurationParameter = require('./ConfigurationParameter');
 
 const configurationParameters = {
   MAXIMUM_ALLOWED_ERROR: new ConfigurationParameter('MAXIMUM_ALLOWED_ERROR', 2),
-  SLEEP_PERIOD: new ConfigurationParameter('SLEEP_PERIOD', 30),
 };
 
 module.exports = configurationParameters;
