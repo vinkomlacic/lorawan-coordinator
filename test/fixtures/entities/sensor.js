d207371ee085503939f22412b78f1a5321eacd70
Vinko Mlačić <vinkomlacic@outlook.com>
2019-07-20 19:24:42 +0200

    test: fix sensor test fixture

--- test/fixtures/entities/sensor.js
+++ test/fixtures/entities/sensor.js
@@ -1,17 +1,28 @@
 'use strict';
 const faker = require('faker');
 
-module.exports = function makeFakeSensor(overrides = {}) {
-  const validNodeStatusValues = ['ACTIVE', 'PASSIVE'];
-  const sensor = {
-    lastGatewayTime: new Date(Date.now() - faker.random.number(120) * 1000),
-    nextGatewayTime: new Date(Date.now() + faker.random.number(60) * 1000),
-    devId: faker.lorem.words(2).concat('-'),
-    nodeStatus: faker.random.arrayElement(validNodeStatusValues),
-  };
+module.exports = function buildMakeFakeSensor({makeFakeSensorConfig, makeFakeSensorData}) {
+  return function makeFakeSensor(overrides = {}) {
+    const validNodeStatusValues = ['ACTIVE', 'PASSIVE'];
 
-  return {
-    ...sensor,
-    ...overrides,
+    const sensorDataCount = faker.random.number(5);
+    const sensorData = [];
+    for (let i = 0; i < sensorDataCount; i++) {
+      sensorData.push(makeFakeSensorData());
+    }
+
+    const sensor = {
+      lastGatewayTime: new Date(Date.now() - faker.random.number(120) * 1000),
+      nextGatewayTime: new Date(Date.now() + faker.random.number(60) * 1000),
+      devId: `${faker.hacker.adjective()}-${faker.hacker.noun()}`,
+      nodeStatus: faker.random.arrayElement(validNodeStatusValues),
+      sensorData,
+      sensorConfig: makeFakeSensorConfig(),
+    };
+
+    return {
+      ...sensor,
+      ...overrides,
+    };
   };
 };
